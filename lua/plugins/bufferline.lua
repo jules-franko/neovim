ba3878c0c261484240e23f88e839999b30669f7c
julesfranko <julesfranko@proton.me>
2025-04-25 11:50:59 -0600

    fixed buffers

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,7 +1,8 @@
 return {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons',
   keys = function()
     return {
-      { "<leader>b", "<cmd><cr>", desc = "Buffer" },
+      { "<leader>bd", "<cmd>:bd<cr>", desc = "Delete Buffer" },
+      { "<leader>n", "<cmd>:bnext<cr>", desc = "Next Buffer" },
     }
   end,
 }
